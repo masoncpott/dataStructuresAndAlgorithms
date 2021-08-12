6acfc0698dffe09ac19c5535d195bbd8db6d8463
Mason Pott <masoncpott@gmail.com>
2021-08-11 20:09:06 -0700

    countUniqueValues

---
+++ problemSolvingPatterns/example_5.js
@@ -0,0 +1,19 @@
+//countUniqueValues
+
+//implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will alsways be sorted.
+//O(n)
+function countUniqueValues(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  var i = 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]){
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+console.log(countUniqueValues([1, 1, 1, 2, 3, 4, 5, 5, 7]));
